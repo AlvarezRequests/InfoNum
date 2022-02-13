c4e653e55e3f9379ebe3d4ba837799df821124ca
Mercury <99209570+AlvarezRequests@users.noreply.github.com>
2022-02-13 00:20:25 -0300

    Create InfoNum.py

---
+++ InfoNum.py
@@ -0,0 +1,36 @@
+import requests
+from getpass import _raw_input
+from colorama import Fore, init
+
+
+api = '510f8c00e437e5c107b8629d2fe5ca20'
+
+print("""
+
+         
+
+           '       \
+          |,.  ,-.  |
+          |()L( ()| |
+          |,'  `".| |
+          |.___.',| `
+         .j `--"' `  `.
+        / '        '   \
+       / /          `   `.
+      / /            `    .
+     / /              l   |
+    . ,               |   |
+    ,"`.             .|   |
+ _.'   ``.          | `..-'l
+|       `.`,        |      `.
+|         `.    __.j         )
+|__        |--""___|      ,-'
+   `"--...,+
+   
+     """)
+numero = int(_raw_input("Ingrese el numero de telefono porfavor >> "))
+
+info = requests.get("http://apilayer.net/api/validate?access_key=%s&number=%s&country_code&format=1" % (api, numero))
+
+for key, valor in info.json().items():
+    print("\n%s: %s" % (key, valor))
